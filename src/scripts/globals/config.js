627ad8ff41d03272360a8125ecd915501d27e8fa
Zhafran Afif <91825801+zhafranafif@users.noreply.github.com>
2022-08-12 17:16:48 +0700

    updating base url and base image url

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-  BASE_URL: 'https://api.npoint.io/5a17b6c3d44bfad564f1/',
-  BASE_IMAGE_URL: 'https://erickbdn.github.io/image-hosting/',
+  BASE_URL: 'https://api.npoint.io/4403eb82535f6ccec880',
+  BASE_IMAGE_URL: 'https://zhafranafif.github.io/Image-Hosting-PI/',
   BASE_COVID_URL: 'https://data.covid19.go.id/public/api/',
   CACHE_NAME: 'GemFinder-V1',
   DATABASE_NAME: 'venue-catalogue-database',
